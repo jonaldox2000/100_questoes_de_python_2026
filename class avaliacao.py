0873d158673d116fcb15381a8f9d9162b7dd0b0e
jonaldox2000 <jonaldox2000@gmail.com>
2026-06-15 17:33:24 -0300

    Add files via upload

---
+++ class avaliacao.py
@@ -0,0 +1,5 @@
+class avalicao:
+    def __init__(self, cliente, nota):
+        self.cliente = cliente
+        self.nota = nota
+    
--- class_restaurante.py
+++ class_restaurante.py
@@ -1,11 +1,14 @@
 class Restaurante:
+    restaurantes = []
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.cardapio = []
-        self.reserva = 10
+        Restaurante.restaurantes.append(self)
+    def __str__(self):
+    
+        return f'{self.nome} | {self.categoria}'
 
 alfredus = Restaurante('Alfredus', 'Pizzaria')
 
-
-print(alfredus.categoria)
+print(alfredus)
